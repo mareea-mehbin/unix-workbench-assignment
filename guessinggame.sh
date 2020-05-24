924a8568bd42bc807389f2f15548d4a291393d5b
mareea-mehbin <mareea22@gmail.com>
2020-05-25 00:33:16 +0530

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# File: test.sh
+
+function guess {
+
+local count=$(ls . | wc -w) 
+local condition=1
+local answ=0
+
+while [[ $condition -eq 1 ]]
+do
+	echo "guess the number of files in this directory?"
+	read answ
+	if [[ $answ -eq $count ]]
+		
+		then condition=0
+		echo "Congratulations! Correct answer!"
+	elif [[ $answ -lt $count ]]
+		then
+		echo "too low"
+	else
+		echo "too high"
+	fi
+done
+
+
+}
+
+guess
